758173cd55cb33567623ff77f28f4c7e48503be7
Sebastian Buczyński <nnplaya@gmail.com>
2021-12-05 20:57:59 +0100

    Add scripts for running the app

---
+++ mbex/run_without_reload.py
@@ -0,0 +1,7 @@
+import uvicorn
+
+from mbex.main import initialize
+
+if __name__ == '__main__':
+    app = initialize()
+    uvicorn.run(app="mbex.main:initialize", host="127.0.0.1", port=8000, reload=False)
